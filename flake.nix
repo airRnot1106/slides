782322d820c9851ea08519d7f5fca34b563a0dd3
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-06-17 18:08:10 +0900

    feat(build): support assets directories via virtual paths

--- flake.nix
+++ flake.nix
@@ -118,6 +118,12 @@
               typstOpts = {
                 root = ".";
               };
+              virtualPaths =
+                commonArgs.virtualPaths
+                ++ lib.optional (builtins.pathExists (slidesDir + "/${name}/assets")) {
+                  src = slidesDir + "/${name}/assets";
+                  dest = "slides/${name}/assets";
+                };
             };
 
             handout-build = typixLib.buildTypstProjectLocal (
